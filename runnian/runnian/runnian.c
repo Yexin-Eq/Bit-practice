feccfcaea59233f03d17936ca6c06aa2a100c321
Yexin-Eq <574956948@qq.com>
2020-11-13 20:32:31 +0800

    闰年-NEW

--- runnian/runnian/runnian.c
+++ runnian/runnian/runnian.c
@@ -1,26 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-int runnian(int x){
-	if (x % 100 == 0){
-		if (x % 400 == 0){
+int Isleapyear(int year){
+	if (year % 100 == 0){
+		//世纪年
+		if (year % 400 == 0){
 			return 1;
 		}
+		else{
+			return 0;
+		}
 	}
-	else if (x % 4 == 0){
+	else //普通年{
+	if (year % 4 == 00){
 		return 1;
 	}
-	return 0;
+	else{
+		return 0;
+	}
+
 }
 
 
 int main()
 {
-	int i = 1000;
-	while (i<2000){
-		if (runnian(i) == 1){
-			printf("%d  ", i);
-		}
-		i = i + 1;
+	int year = 1000;
+	while (year <= 2000){
+
+		if (Isleapyear(year) == 1){
+			printf("%d ", year);
+
+		}year += 1;
 	}return 0;
 }
