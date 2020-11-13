bc52e452fbc450de7f32d96d043ece7f98a107e8
Yexin-Eq <574956948@qq.com>
2020-11-13 20:24:22 +0800

    乘法口诀表NEW

--- chengfa/chengfa/chengfa.c
+++ chengfa/chengfa/chengfa.c
@@ -1,17 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+
+void Printline(int line){
+	int col = 1;
+	while (col <= line){
+		printf("%d*%d=%d ", col, line, col*line);
+		col = col + 1;
+	}printf("\n");
+}
+
 int main()
 {
-	int line = 1;
-	while (line < 10){
-
-		int col = 1;
-		while (col <= line){
-			printf("%d*%d=%d ", col, line, col*line);
-			col = col + 1;
-		}line = line + 1;
-		printf("\n");
+	int i = 1;
+	while (i < 10){
+		Printline(i);
+		i = i + 1;
 	}
-
 	return 0;
 }
