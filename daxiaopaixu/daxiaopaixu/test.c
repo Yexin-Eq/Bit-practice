c36a99acef8496a8d2347deed61d37a9540f7889
Yexin-Eq <574956948@qq.com>
2021-06-20 08:11:37 +0800

    1

--- daxiaopaixu/daxiaopaixu/test.c
+++ daxiaopaixu/daxiaopaixu/test.c
@@ -1,27 +1,54 @@
 //将三个数从大到小输出
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main(){
-	printf("请输入三位数;");
+//int main(){
+//	printf("请输入三位数;");
+//	int arr[3] = { 0 };
+//	scanf("%d %d %d", &arr[0], &arr[1], &arr[2]);
+//	int max = 0;
+//	int min = 0;
+//	if (arr[0] > arr[1]){
+//		max = arr[0];
+//		min = arr[1];
+//	}else{
+//		max = arr[1];
+//		min = arr[0];
+//	}
+//	if (arr[2] > max){
+//		printf("%d %d %d", arr[2], max, min);
+//	}
+//	else if (arr[2] < min){
+//		printf("%d %d %d", max, min, arr[2]); 
+//	}
+//	else{
+//		printf("%d %d %d", max, arr[2], min);
+//	}
+//	return 0;
+//}
+
+
+int mian(){
+	printf("请输入三个数组:");
 	int arr[3] = { 0 };
-	scanf("%d %d %d", &arr[0], &arr[1], &arr[2]);
+	scanf("%d %d %d", arr[0], arr[1], arr[2]);
 	int max = 0;
 	int min = 0;
 	if (arr[0] > arr[1]){
 		max = arr[0];
 		min = arr[1];
-	}else{
-		max = arr[1];
+	}
+	else{
 		min = arr[0];
+		max = arr[1];
 	}
 	if (arr[2] > max){
 		printf("%d %d %d", arr[2], max, min);
 	}
 	else if (arr[2] < min){
-		printf("%d %d %d", max, min, arr[2]); 
+		printf("%d %d %d",  max, min,arr[2]);
 	}
 	else{
 		printf("%d %d %d", max, arr[2], min);
-	}
-	return 0;
+	}return 0;
+
 }
